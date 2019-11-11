270009a470e3ecfbf42e465c6337da8e442a7c88
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-11-11 20:58:33 +0900

    Edit Controllers
    #What
    作成済みのデバイスコントローラー設定の修正
    #Why
    ログイン時にエラーを吐いたため、修正。
    
    #修正ファイル一覧
    application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_acrtion :configure_permitted_parameters , if: devise_controller?
 
-  def configure_permitted
-    devise_parameter_sanitizer.permit(sign_up, keys: [:nickname, :family_name, :first_name, :family_name_cana, :first_name_cana, :birthday, :telphone, :profile, :image, :point])
+  before_action :configure_permitted_parameters , if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_cana, :first_name_cana, :birthday, :telphone, :profile, :image, :point])
   end
 end
