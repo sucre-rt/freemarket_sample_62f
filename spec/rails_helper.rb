00e7be2a4f6242dc6f1bac1f8319bbff42caa84d
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-11-12 16:48:06 +0900

    FactoryBot_Setting
    #What
    FactoryBotの導入とConfig適用
    
    #Why
    テスト実施時の負担軽減のため導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  
+  #FactoryBot記述省略用コンフィグ
+  config.include FactoryBot::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
