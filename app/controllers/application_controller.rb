bc52f8e5be54fc0aed4cac16ef278121c08eceb8
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-16 15:54:05 +0900

    basic_repaire

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters , if: :devise_controller?
   before_action :basic_auth, if: :production?
-  before_action :basic_auth
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_cana, :first_name_cana, :birthday, :telphone, :profile, :image, :point])
