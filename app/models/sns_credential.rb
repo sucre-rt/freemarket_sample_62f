be7f29fdb73b9a5983747429ab36604f687372df
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-17 19:50:53 +0900

    repaire validete

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,6 +1,6 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user
 
-  validates :uid, presence: true, unipue: true
-  validates :provider, presence: true, unipue: true
+  validates :uid, presence: true, uniqueness: true
+  validates :provider, presence: true, uniqueness: true
 end
