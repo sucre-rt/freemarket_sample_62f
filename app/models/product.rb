f5e1012e5a25f58ee55098595c898d00edc856c3
hisa66 <hisanori.koike17.193@gmail.com>
2019-11-17 16:12:41 +0900

    productsテーブル作成中

---
+++ app/models/product.rb
@@ -0,0 +1,20 @@
+class Product < ApplicationRecord
+  validates :name, presence: true, length: {in: 1..40}
+  validates :information, presence: true, length: {in: 1..1000}
+  validates :price, presence: true, inclusion: { in: (300..9999999)}
+  validates :area, presence: true
+  validates :status, presence: true
+  validates :sending_days, presence: true
+  validates :user, presence: true
+
+  has_one :evaluation
+  has_one :payment
+  has_many :likes, dependetnt: :destroy
+  has_many :transactions
+  has_many :messages
+  has_many :images, dependent: :destroy
+  belongs_to :users
+  belongs_to :category
+  belongs_to :delivery
+  belongs_to :brand
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_102225) do
+ActiveRecord::Schema.define(version: 2019_11_17_032750) do
+
+  create_table "products", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "information", null: false
+    t.integer "price", null: false
+    t.string "area", null: false
+    t.string "status", null: false
+    t.string "sending_days", null: false
+    t.integer "profit", null: false
+    t.string "selling_status", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -34,4 +47,5 @@ ActiveRecord::Schema.define(version: 2019_11_12_102225) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
