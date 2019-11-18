9d10b0ba7ecb085ea8323300acd6822b4852102e
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-11-18 12:45:57 +0900

    commit2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.11.2'
 
 # credentials.yml.enc用のシンボリックリンクを追加
-set :linked_files, %w{ config/secrets.yml }
+# set :linked_files, %w{ config/secrets.yml }
 
 # Capistranoのログの表示に利用する
 set :application, 'freemarket_sample_62f'
@@ -41,16 +41,4 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-
-  desc 'upload secrets.yml'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
 end
