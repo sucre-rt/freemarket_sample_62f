820cacc71e42691c9f0093e95e340088fefd0f20
hisa66 <hisanori.koike17.193@gmail.com>
2019-11-19 11:36:49 +0900

    productsテーブルとコントローラ作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
 
-  def sell
+  def sell                                          #商品出品画面にいく
+    @product = Product.new                          #productの中のカラムを作り、メソッドに入れる
   end
 
+  def create                                        #
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :information, :price, :area, :status, :sending_days, :profit, :selling_status).merge(user_id: 1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,10 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :products do
+    collection do
+      get 'sell'
+    end
+  end
+
 end
