ab20f2908656022784663a45fbe346a5d8ba1c6a
hisa66 <hisanori.koike17.193@gmail.com>
2019-11-19 11:36:49 +0900

    productsテーブルとコントローラ作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,4 +39,13 @@ private
     ).merge(user_id: 1)
   end
 
+  def create                                        #
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :information, :price, :area, :status, :sending_days, :profit, :selling_status).merge(user_id: 1)
+  end
 end
