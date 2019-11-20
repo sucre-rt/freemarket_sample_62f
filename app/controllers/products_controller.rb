6c01109a8f2e4e99e5571a03d45a593494f5f174
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-11-20 20:04:48 +0900

    Multiple tables Create Function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
   def sell_test
     @product = Product.new
     @product.images.build
+
   end
 
   def create
@@ -39,13 +40,4 @@ private
     ).merge(user_id: 1)
   end
 
-  def create                                        #
-    @product = Product.new(product_params)
-    @product.save
-  end
-
-private
-  def product_params
-    params.require(:product).permit(:name, :information, :price, :area, :status, :sending_days, :profit, :selling_status).merge(user_id: 1)
-  end
 end
