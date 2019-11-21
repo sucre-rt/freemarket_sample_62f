0e822574a79890528111e94ba5b42df848637c90
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-21 15:00:41 +0900

    set prefecture select data

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,27 +2,33 @@ class AddressesController < ApplicationController
 
   # マイページ→本人情報ページ
   def edit
-    @prefecture = ""
+    @prefecture = Address.set_prefecture
     @user = current_user
     @birthday = @user.birthday.strftime('%Y/%m/%d')
-    @address = Adress.find_by(user_id: @user.id)
+    @address = Address.find_by(user_id: @user.id)
   end
 
   def update
-    @address = Adress.find_by(user_id: current_user.id)
-    @address.update(address_update_params)
+    @address = Address.find_by(user_id: current_user.id)
+    if @address.update(address_update_params)
+      flash[:notice] = "変更しました。"
+      redirect_to identification_addresses_path
+    else 
+      flash[:notice] = "変更に失敗しました。"
+      redirect_to identification_addresses_path
+    end
   end
 
   private
 
   def address_update_params
-    params.require(:adress).permit(
+    params.require(:address).permit(
       :family_name,
       :first_name,
       :family_name_cana,
       :first_name_cana,
       :postal_code,
-      :prefenture,
+      :prefecture,
       :city,
       :address,
       :building,
