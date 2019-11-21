5b37042aa0c3b607ca58b6ba4b23d1eacceeaa6b
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-21 15:26:38 +0900

    add column users-information-address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_043336) do
+ActiveRecord::Schema.define(version: 2019_11_21_061932) do
 
   create_table "addresses", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -75,6 +75,11 @@ ActiveRecord::Schema.define(version: 2019_11_21_043336) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "postal_code"
+    t.string "prefecture"
+    t.string "city"
+    t.string "address"
+    t.string "building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
