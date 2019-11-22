823b317ddaaa113045ab1fa33d044b212b2489b6
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-11-22 19:13:12 +0900

    correction valid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_091444) do
+ActiveRecord::Schema.define(version: 2019_11_22_082112) do
 
   create_table "addresses", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
