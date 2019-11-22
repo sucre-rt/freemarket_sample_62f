02bb306f93f43f3292a2b2fee3f370720715b07a
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-22 18:30:10 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -29,39 +29,9 @@ ActiveRecord::Schema.define(version: 2019_11_22_082112) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table "brands", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "path", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "category_sizes", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "size_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_category_sizes_on_category_id"
-    t.index ["size_id"], name: "index_category_sizes_on_size_id"
-  end
-
-  create_table "deliveries", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "responsibility", null: false
-    t.string "way", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "images", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
     t.integer "product_id", null: false
-    t.string "images"
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
@@ -75,20 +45,7 @@ ActiveRecord::Schema.define(version: 2019_11_22_082112) do
     t.integer "profit", null: false
     t.string "selling_status", null: false
     t.integer "user_id", null: false
-    t.integer "category_id", null: false
-    t.integer "delivery_id", null: false
-    t.integer "brand_id", null: false
-    t.index ["brand_id"], name: "index_products_on_brand_id"
-    t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["delivery_id"], name: "index_products_on_delivery_id"
     t.index ["user_id"], name: "index_products_on_user_id"
-  end
-
-  create_table "sizes", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "path", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "sns_credentials", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -128,11 +85,7 @@ ActiveRecord::Schema.define(version: 2019_11_22_082112) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "adresses", "users"
   add_foreign_key "images", "products"
-  add_foreign_key "products", "brands"
-  add_foreign_key "products", "categories"
-  add_foreign_key "products", "deliveries"
   add_foreign_key "products", "users"
   add_foreign_key "sns_credentials", "users"
 end
