8702eae91eabeed4a9cf4e380c1f4de5faf5960d
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-25 18:56:18 +0900

    set selectbox data

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,6 +1,10 @@
 class CardController < ApplicationController
 
   def new
+    @years = []    
+    Date.today.year.upto(Date.today.year + 10){ |year|
+      @years << year
+    }
   end
 
   def create
