24ef6512a544ebf7dfae330d070d0a33b193d587
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-26 21:20:06 +0900

    halfway create card in signup

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,6 +1,5 @@
+// 生年月日取得
 $(function(){
-
-  // 生年月日取得
   $('.user-form__content__group__user-select').find('.user-select-default').change(function(){
     let year = $('#user-birthday-year').val();
     let month = $('#user-birthday-month').val();
@@ -8,5 +7,25 @@ $(function(){
     let birthday = year + month + day
     $('#user_birthday').val(birthday)
   });
+})
+
+// カード情報の取得
+$(function(){
+
+  Payjp.setPublicKey('pk_test_34d7bf26ffb0a19c8c6ad2e1'); // payjp公開鍵
+
+  let form = $("#signup-card-form");
+
+  $(form).on("click", "#signup-token-submit", function(e){
+    e.preventDefault();
+    form.find("input[type=submit]").prop("disabled", true);
+
+    let card = {
+      number:     parseInt($("#signup-card-number").val()), // 入力されたカード情報の文字列を整数に変換して取得
+      exp_month:  parseInt($("#signup-exp-month").val()),
+      exp_year:   parseInt($("#signup-exp-year").val()),
+      cvc:        parseInt($("#signup-cvc").val())
+    };
+  })
 
 })
