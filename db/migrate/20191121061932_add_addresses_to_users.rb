78dbdf1ca6530be54313bbab22224899f36470ae
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-11-27 12:36:32 +0900

    Ancestry Setting

--- db/migrate/20191121061932_add_addresses_to_users.rb
+++ db/migrate/20191121061932_add_addresses_to_users.rb
@@ -1,4 +1,4 @@
-class AddAddressesToUsers < ActiveRecord::Migration[5.2]
+class AddAddressesToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :postal_code, :string
     add_column :users, :prefecture, :string
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#親
+lady, men = Category.create([{name: "レディース"}, {name: "メンズ"}])
+
+#子
+lady_tops, lady_jacket = lady.children.create([{name: "トップス"}, {name: "ジャケット/アウター"}])
+men_tops, men_jacket = men.children.create([{name: "トップス"}, {name: "ジャケット/アウター"}])
+
+#孫
+shirt_short, shirt_long = lady_tops.children.create([{name: "Tシャツ/カットソー(半袖/袖なし)"}, {name: "Tシャツ/カットソー(七分/長袖)"}])
+shirt_short, shirt_long = men_tops.children.create([{name: "Tシャツ/カットソー(半袖/袖なし)"}, {name: "Tシャツ/カットソー(七分/長袖)"}])
+tailored_jacket, collarless_jacket = lady_jacket.children.create([{name: "テーラードジャケット"}, {name: "ノーカラージャケット"}])
+tailored_jacket, collarless_jacket = men_jacket.children.create([{name: "テーラードジャケット"}, {name: "ノーカラージャケット"}])
