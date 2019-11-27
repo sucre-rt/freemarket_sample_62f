252c3e55f26fabb5ed0bb3ff5a2885eed86505b3
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-11-27 21:29:20 +0900

    Production_status function

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+
+  def size
+  end
+
+  def status
+    [{name: "新品、未使用"}, {name: "未使用に近い"}, {name: "目立った傷や汚れなし"},{name: "やや傷や汚れあり"}, {name: "傷や汚れあり"}, {name: "全体的に状態が悪い"}]
+  end
 end
