a411052d926c2fe2174a15f5c3c69a97955d058f
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-11-27 12:48:20 +0900

    delete commentout

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -8,24 +8,3 @@ $(function(){
     $('#user_birthday').val(birthday)
   });
 })
-
-// カード情報の取得
-// $(function(){
-
-//   Payjp.setPublicKey('pk_test_34d7bf26ffb0a19c8c6ad2e1'); // payjp公開鍵
-
-//   let form = $("#signup-card-form");
-
-//   $(form).on("click", "#signup-token-submit", function(e){
-//     e.preventDefault();
-//     form.find("input[type=submit]").prop("disabled", true);
-
-//     let card = {
-//       number:     parseInt($("#signup-card-number").val()), // 入力されたカード情報の文字列を整数に変換して取得
-//       exp_month:  parseInt($("#signup-exp-month").val()),
-//       exp_year:   parseInt($("#signup-exp-year").val()),
-//       cvc:        parseInt($("#signup-cvc").val())
-//     };
-//   })
-
-// })
