00318edcd96881b0fbc6521158cf14f2216748c8
hisa66 <hisanori.koike17.193@gmail.com>
2019-11-29 12:11:27 +0900

    商品出品確認画面の削除機能実装

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ApplicationRecord
 
-  validates :card_id, presence: true
-  validates :customer_id, presence: true
+  # validates :card_id, presence: true
+  # validates :customer_id, presence: true
 
   belongs_to :user
 
