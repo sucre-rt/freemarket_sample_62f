c8ef14cefb7f2f63e183360dae68b51547d0b507
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-12-04 13:25:44 +0900

    repair set category  variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,32 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters , if: :devise_controller?
   before_action :basic_auth, if: :production?
+  before_action :set_category_products
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_cana, :first_name_cana, :birthday, :telphone, :profile, :image, :point])
+  end
+
+  def set_category_products
+    @products = Product.all.order("id DESC")
+    @women = []
+    @men = []
+    @parents = Category.all.limit(13)
+    @home_appliances = []
+    @toys = []
+
+    @products.each do |product|
+      case product.category.parent.parent.name
+      when "レディース"
+        @women << product
+      when "メンズ"
+        @men << product
+      when "家電・スマホ・カメラ"
+        @home_appliances << product
+      when "おもちゃ・ホビー・グッズ"
+        @toys << product
+      end
+    end
   end
 
   private
