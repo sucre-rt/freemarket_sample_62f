6229d52643b2ecbda34b5f3e1d1ad4b84dbc0cf6
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-12-04 21:28:21 +0900

    crrection commit

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,6 +6,33 @@ crumb :mypage do
   link "マイページ", mypage_index_path
 end
 
+crumb :like do
+  link "いいね一覧", like_mypage_path
+  parent :mypage
+end
+
+crumb :profile do
+  link "プロフィール", profile_signup_path
+  parent :mypage
+end
+
+crumb :pay do
+  link "支払い方法", card_new_path
+  parent :mypage
+end
+
+crumb :acount do
+  link "本人情報の登録", edit_user_registration_path
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", destroy_user_session_path
+  parent :mypage
+end
+
+
+
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
 # files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
